4f093b399b54b335e9b1c4790ff130133cca4d94
Mateus de Moraes <mateus.moraes123@hotmail.com>
2023-06-15 18:04:11 -0300

    Trabalhando no swagger

--- src/models/Rota.js
+++ src/models/Rota.js
@@ -3,13 +3,13 @@ import mongoosePaginate from "mongoose-paginate-v2";
 
 const rotaSchema = new mongoose.Schema({
   rota: { type: String, required: [true, "Rota é obrigatório"], trim: true, index: true },
-  dominio: { type: String, required: true, trim: true, index: true },
-  ativo: { type: Boolean, required: true },
-  verbo_get: { type: Boolean, required: true },
-  verbo_post: { type: Boolean, required: true },
-  verbo_put: { type: Boolean, required: true },
-  verbo_patch: { type: Boolean, required: true },
-  verbo_delete: { type: Boolean, required: true }
+  dominio: { type: String, required: [true, "Domínio é obrigatório"], trim: true, index: true },
+  ativo: { type: Boolean, required: [true, "Ativo é obrigatório"] },
+  verbo_get: { type: Boolean, required: [true, "Verbo Get é obrigatório"] },
+  verbo_post: { type: Boolean, required: [true, "Verbo Post é obrigatório"] },
+  verbo_put: { type: Boolean, required: [true, "Verbo Put é obrigatório"] },
+  verbo_patch: { type: Boolean, required: [true, "Verbo Patch é obrigatório"] },
+  verbo_delete: { type: Boolean, required: [true, "Verbo Delete é obrigatório"] }
 },
   { versionKey: false },
 );
--- src/routes/rotaRoutes.js
+++ src/routes/rotaRoutes.js
@@ -4,6 +4,66 @@ import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * paths:
+ *  /rotas:
+ *    get:
+ *      tags:
+ *        - Rotas
+ *      summary: Lista todas as rotas
+ *      security:
+ *        - bearerAuth: []
+ *      parameters:
+ *        - in: query
+ *          rota: rota
+ *          schema:
+ *            type: string
+ *          description: Nome da rota para filtrar
+ *        - in: query
+ *          name: page 
+ *          schema:
+ *            type: integer
+ *          description: Qual é a página desejada para a busca
+ *        - in: query
+ *          name: perPage
+ *          schema:
+ *            type: integer
+ *          description: Quantidade de registros por página
+ * 
+ *      responses:
+ *        200:
+ *          description: Retorna a lista de rotas
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  docs:
+ *                    type: array
+ *                    items:
+ *                      $ref: '#/components/schemas/rota'
+ *                  totalDocs:
+ *                    type: integer
+ *                  limit:
+ *                    type: integer
+ *                  totalPages:
+ *                    type: integer,
+ *                  page:
+ *                    type: integer
+ *                  pagingCounter:
+ *                    type: integer
+ *                  hasPrevPage:
+ *                    type: integer
+ *                  hasNextPage:
+ *                    type: integer
+ *                  prevPage:
+ *                    type: integer
+ *                  nextPage:
+ *                    type: integer
+ *          
+ */
+
 router
   .get("/rotas", AuthMiddleware, RotaController.listarRotas)
   .get("/rotas/:id", AuthMiddleware, RotaController.listarPorId)
@@ -13,3 +73,5 @@ router
   .delete("/rotas/:id", AuthMiddleware, RotaController.deletarRota)
 
 export default router;
+
+
